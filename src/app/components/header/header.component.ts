7ece0a5b36dd5c50d6b9f20660e89c071c012e6b
ehvidalp <ehvidalp@gmail.com>
2024-09-28 09:56:42 -0600

    Add configuration translation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,45 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { WorldIconComponent } from '@assets/svg/world/world.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Languages } from '@shared/interfaces/languages.interface';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     CommonModule,
+    TranslateModule,
+    FormsModule,
+    WorldIconComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent { }
+export class HeaderComponent implements OnInit { 
+  private translateService = inject(TranslateService);
+
+  currentLanguage: Languages = Languages.es;
+  listLanguages = signal([
+    Languages.en,
+    Languages.es,
+  ])
+  
+  ngOnInit(): void {
+    this.getDefaultSetting();
+  }
+
+  getDefaultSetting(): void {
+    const deviceLanguage = navigator.language.split('-')[0] ?? this.currentLanguage;
+    this.currentLanguage = deviceLanguage as Languages;
+    this.changeLanguage(undefined, deviceLanguage as Languages);
+  }
+
+  changeLanguage(event?: Event, language?: Languages): void {
+    const lang = language ? language : (event?.target as HTMLSelectElement).value
+    this.translateService.use(lang);
+  }
+
+}
---
+++ src/app/shared/interfaces/languages.interface.ts
@@ -0,0 +1,5 @@
+export enum Languages {
+    en = 'en',
+    es = 'es',
+}
+
