b576495b25f62b9d049d6100f375060657abdceb
ehvidalp <ehvidalp@gmail.com>
2024-10-01 10:10:33 -0600

    Add header styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { HttpClient, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -12,6 +14,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideLottieOptions({ player: () => player }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit, Signal, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { WorldIconComponent } from '@assets/svg/world/world.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Languages } from '@shared/interfaces/languages.interface';
+import { AnimationItem } from 'lottie-web';
+import { LottieComponent, AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +14,8 @@ import { Languages } from '@shared/interfaces/languages.interface';
     CommonModule,
     TranslateModule,
     FormsModule,
-    WorldIconComponent
+    WorldIconComponent,
+    LottieComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
@@ -20,7 +23,16 @@ import { Languages } from '@shared/interfaces/languages.interface';
 })
 export class HeaderComponent implements OnInit { 
   private translateService = inject(TranslateService);
-
+  // Lottie
+  lightDarkIconAnimation: AnimationItem | undefined;
+  lightDarkIcon: AnimationOptions = {
+    path: 'assets/lottie/light-dark-icon.json',
+    loop: false,
+    autoplay: false,
+  };
+  // 
+  currentTheme= signal('dark');
+  isDarkTheme = computed(() => this.currentTheme() === 'dark');
   currentLanguage: Languages = Languages.es;
   listLanguages = signal([
     Languages.en,
@@ -42,4 +54,23 @@ export class HeaderComponent implements OnInit {
     this.translateService.use(lang);
   }
 
+  changeTheme(): void {
+    this.currentTheme.update((theme) => theme === 'dark' ? 'light' : 'dark');
+    this.updateLightDarkIconPosition();
+  }
+
+  animationCreated(animationItem: AnimationItem): void {
+    animationItem.addEventListener('DOMLoaded', () => {
+      this.lightDarkIconAnimation = animationItem;
+      this.updateLightDarkIconPosition();
+    });
+  }
+
+  updateLightDarkIconPosition(): void {
+    if(!this.lightDarkIconAnimation) return;
+    document.body.classList[this.isDarkTheme() ? 'add' : 'remove']('dark-theme');
+    
+    this.lightDarkIconAnimation.setDirection(this.isDarkTheme() ? 1 : -1);
+    this.lightDarkIconAnimation.play();
+  }
 }
