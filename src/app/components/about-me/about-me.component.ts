bac404ee6b541f0e814b556034b292b26eaa4b83
ehvidalp <ehvidalp@gmail.com>
2024-10-03 13:45:54 -0600

    Add about-me component and translate content

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { DropdownComponent } from '@shared/ui/dropdown/dropdown.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [
     CommonModule,
+    TranslateModule,
   ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css',
