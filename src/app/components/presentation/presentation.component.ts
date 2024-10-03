ae90212173a445d0da21899325bb55a546e4ce67
ehvidalp <ehvidalp@gmail.com>
2024-10-03 13:34:44 -0600

    Add translation for presentation and add query of layout

--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { StainComponent } from '@assets/svg/stain/stain.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-presentation',
   standalone: true,
   imports: [
     CommonModule,
-    StainComponent
+    StainComponent,
+    TranslateModule,
   ],
   templateUrl: './presentation.component.html',
   styleUrl: './presentation.component.css',
