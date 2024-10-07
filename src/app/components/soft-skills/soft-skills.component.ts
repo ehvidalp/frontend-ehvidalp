c201cba1ec734464e0dd6b9d44d506b819fc335a
ehvidalp <ehvidalp@gmail.com>
2024-10-07 14:54:50 -0600

    Refactor soft-skills component for responsiveness and consistency

--- src/app/components/soft-skills/soft-skills.component.ts
+++ src/app/components/soft-skills/soft-skills.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-soft-skills',
   standalone: true,
   imports: [
     CommonModule,
+    TranslateModule,
   ],
   templateUrl: './soft-skills.component.html',
   styleUrl: './soft-skills.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SoftSkillsComponent { 
-  softSkills = signal(['Communication', 'Leadership', 'Team work', 'Critical thinking', 'Creative', 'Problem solving']);
+export class SoftSkillsComponent {
+  softSkills = ['communication', 'leadership', 'team_work', 'critical_thinking', 'creative', 'problem_solving']; 
 }
